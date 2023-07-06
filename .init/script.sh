ec37705c8c0f1bd4828a0eeb4d747356d455aea9
hackpig520 <138371687+hackpig520@users.noreply.github.com>
2023-07-06 19:48:10 +0800

    Update script.sh

--- .init/script.sh
+++ .init/script.sh
@@ -1,6 +1,22 @@
 #!/bin/bash
 
 set -e
+
+# Start creating work folders
+mkdir ~/work 
+cat << EOF > ~/README
+您正在使用 HackPig520 提供的免费 Linux 服务器，基于 Github Actions
+---
+你可以创建、修改、删除文件，运行可执行文件或者使用各种CLI
+想要测试网速？键入以下命令
+ $ speedtest
+想要编译源代码？系统预装了 Java Python Golang 语言工具，可开箱即用！
+想要持续连接服务器？创建空文件 '/tmp/hackpig520' 以禁用 5 分钟免费时长限制
+---
+Using it for harmful purposes is extremely forbidden.
+Our team & company is not responsible for its’ usages and consequences.
+EOF
+
 # Install tmate on macOS or Ubuntu
 echo Setting up tmate...
 if [ -x "$(command -v brew)" ]; then
@@ -32,10 +48,10 @@ tmate -S /tmp/tmate.sock display -p '#{tmate_web}'
 echo After connecting you can run 'touch /tmp/hackpig520' to disable the 5m timeout
 echo -e '\e[1;32m________________________________________________________________________________\e[m'
 
-# Wait for connection to close or timeout in 5 min
+# Wait for connection to close or timeout in 15 min
 timeout=$((5*60))
 while [ -S /tmp/tmate.sock ]; do
-  sleep 10
+  sleep 5
   timeout=$(($timeout-5))
   echo -e '\e[1;32m-----------------------------\e[m'
   echo To connect to this session copy-and-paste the following into a terminal:
